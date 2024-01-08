98110f38e30a4890787c77e23c5cc2b8d544e93b
bog500 <limbo_9@hotmail.com>
2024-01-08 15:21:51 -0500

    Add project files.

---
+++ FolderStructureDupplicator/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FolderStructureDupplicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolderSource = new System.Windows.Forms.TextBox();
+            this.txtFolderDest = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFolderSource
+            // 
+            this.txtFolderSource.Location = new System.Drawing.Point(64, 72);
+            this.txtFolderSource.Name = "txtFolderSource";
+            this.txtFolderSource.Size = new System.Drawing.Size(403, 20);
+            this.txtFolderSource.TabIndex = 0;
+            this.txtFolderSource.Text = "J:\\Folder1";
+            // 
+            // txtFolderDest
+            // 
+            this.txtFolderDest.Location = new System.Drawing.Point(64, 140);
+            this.txtFolderDest.Name = "txtFolderDest";
+            this.txtFolderDest.Size = new System.Drawing.Size(396, 20);
+            this.txtFolderDest.TabIndex = 1;
+            this.txtFolderDest.Text = "J:\\Folder2";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(494, 72);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(113, 88);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start dupplication";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 247);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtFolderDest);
+            this.Controls.Add(this.txtFolderSource);
+            this.Name = "Form1";
+            this.Text = "FolderStructureDupplicator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolderSource;
+        private System.Windows.Forms.TextBox txtFolderDest;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FolderStructureDupplicator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderStructureDupplicator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ((Control)sender).Enabled = false;
+            DirectoryInfo diSrc = new DirectoryInfo(txtFolderSource.Text);
+            DirectoryInfo diDes = new DirectoryInfo(txtFolderDest.Text);
+            Explore(diSrc, diDes);
+            ((Control)sender).Enabled = true;
+            MessageBox.Show("Completed");
+        }
+
+        private void Explore(DirectoryInfo diSrc, DirectoryInfo diDes)
+        {
+            if(diDes.Exists == false)
+            {
+                diDes.Create();
+            }
+
+            foreach(var sub in diSrc.GetDirectories())
+            {
+                string subDesStr = Path.Combine(diDes.FullName, sub.Name);
+                DirectoryInfo subDes = new DirectoryInfo(subDesStr);
+                Explore(sub, subDes);
+            }
+        }
+    }
+}
